1c2e47509c592cd4377f1918946c85948effe6e6
Rifkhan Mohamed <rifkhanabdullah@gmail.com>
2024-02-13 11:31:03 +0000

    download cv changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,6 +5,23 @@ import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
+  const handleDownload = () => {
+    // Replace 'your-google-drive-link' with the actual link to your GIF on Google Drive
+    const googleDriveLink = `${getImageUrl("hero/Mohamed-Rifkhan-CV.pdf")}`;
+    const fileName = googleDriveLink.substring(googleDriveLink.lastIndexOf('/') + 1);
+    // Create an invisible link element
+    const link = document.createElement('a');
+    link.href = googleDriveLink;
+    link.target = '_blank';
+    link.download = fileName; // Specify the desired file name
+
+    // Trigger a click event on the link
+    document.body.appendChild(link);
+    link.click();
+
+    // Remove the link from the DOM
+    document.body.removeChild(link);
+  };
   return (
     <section className={styles.container}>
       <div className={styles.content}>
@@ -13,9 +30,14 @@ export const Hero = () => {
           I'm a full-stack developer with over 3 years of experience using React, Angular, Spring Boot (Java) and
           .Net (c#). Reach out if you'd like to learn more!
         </p>
-        <a href="mailto:ar.mohamed.rifkhan@gmail.com" className={styles.contactBtn}>
-          Contact Me
-        </a>
+        <div className={styles.buttonContainer}>
+          <a href="mailto:ar.mohamed.rifkhan@gmail.com" className={styles.contactBtn}>
+            Contact Me
+          </a>
+          <div>
+            <button className={styles.gifButton}  onClick={handleDownload}></button>
+          </div>
+        </div>
       </div>
       <img
         src={getImageUrl("hero/heroImage.png")}
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, {useState,useEffect} from "react";
 // eslint-disable-next-line no-unused-vars
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -11,15 +11,40 @@ import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
+    const [slidesToShow, setSlidesToShow] = useState(getInitialSlidesToShow());
+
     const sliderSettings = {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow: slidesToShow,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1500,
       };
+      function getInitialSlidesToShow() {
+        const screenWidth = window.innerWidth;
+        if (screenWidth <= 830 && screenWidth >= 600) {
+            return 2; // Show 1 card on smaller screens
+          } else if (screenWidth <= 600) {
+            return 1; // Show 3 cards on larger screens
+          }
+          else {
+            return 3;
+          }
+      }
+    
+      useEffect(() => {
+        const handleResize = () => {
+          setSlidesToShow(getInitialSlidesToShow());
+        };
+    
+        window.addEventListener("resize", handleResize);
+    
+        return () => {
+          window.removeEventListener("resize", handleResize);
+        };
+      }, []);
   return (
         <section className={styles.container} id="projects">
         <h2 className={styles.title}>Projects</h2>
