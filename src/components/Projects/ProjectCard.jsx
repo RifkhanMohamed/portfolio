812a978b8017d605631dbaa197d1851a3ed87f20
Rifkhan Mohamed <rifkhanabdullah@gmail.com>
2024-02-21 15:43:20 +0000

    project card update

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,14 +1,56 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState,useEffect } from "react";
 
 import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
-
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 export const ProjectCard = ({
   // eslint-disable-next-line react/prop-types
-  project: { title, imageSrc, description, skills, source },
+  project: { title, imageSrc, description, skills, source, demo },
 }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  // const [slidesToShow, setSlidesToShow] = useState(getInitialSlidesToShow());
+
+  const sliderSettings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+    };
+    // function getInitialSlidesToShow() {
+    //   const screenWidth = window.innerWidth;
+    //   if (screenWidth <= 830 && screenWidth >= 600) {
+    //       return 2; // Show 1 card on smaller screens
+    //     } else if (screenWidth <= 600) {
+    //       return 1; // Show 3 cards on larger screens
+    //     }
+    //     else {
+    //       return 3;
+    //     }
+    // }
+
+    // useEffect(() => {
+    //   const handleResize = () => {
+    //     setSlidesToShow(getInitialSlidesToShow());
+    //   };
+  
+    //   window.addEventListener("resize", handleResize);
+  
+    //   return () => {
+    //     window.removeEventListener("resize", handleResize);
+    //   };
+    // }, []);
   return (
     <div className={styles.container}>
       <img
@@ -28,12 +70,52 @@ export const ProjectCard = ({
         })}
       </ul>
       <div className={styles.links}>
-        <a href="#" className={styles.link}>
-          Demo
-        </a>
-        <a href={source} className={styles.link}>
+      <Button variant="primary" onClick={handleShow}>
+        Demo
+      </Button>
+      <Modal
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      show={show} onHide={handleClose}
+    >
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Slider {...sliderSettings}  className={styles.projectsSlider}>
+          {demo.map((media, id) => (
+                <div key={id}>
+                  {media.endsWith(".mp4") ? (
+                    <video
+                      controls
+                      width="100%"
+                      height="100%"
+                      className={styles.imageSlider}
+                    >
+                      <source src={getImageUrl(media)} type="video/mp4" />
+                      Your browser does not support the video tag.
+                    </video>
+                  ) : (
+                    <img
+                      src={getImageUrl(media)}
+                      alt={`Demo Image ${id + 1}`}
+                      className={styles.imageSlider}
+                    />
+                  )}
+                </div>
+              ))}
+          </Slider>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+        <Button variant="primary" onClick={() => window.open(source, "_blank")}>
           Source
-        </a>
+        </Button>
       </div>
     </div>
   );
